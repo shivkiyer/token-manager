1e4af8e9d352b43c589f30547d193e4ea6dd2ddb
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-09-03 11:22:03 -0400

    Refactoring db setup code

--- server/server.js
+++ server/server.js
@@ -1,33 +1,20 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
 const routes = require('./../routes/routes');
-const { Sequelize } = require('sequelize');
+const sequelize = require('../db/config/config');
 
 app.use('/api/', routes);
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: 5432,
-  }
-);
-
-/**
- * Test connection to database and start server
- */
-const initSequelize = async () => {
-  try {
-    await sequelize.authenticate();
-    const server = app.listen(8000, () =>
-      console.log('Listening on port 8000')
+// Test connection to database and start server
+sequelize
+  .authenticate()
+  .then(() => {
+    const server = app.listen(process.env.DEV_SERVER_PORT, () =>
+      console.log(`Listening on port ${process.env.DEV_SERVER_PORT}`)
     );
-  } catch (e) {
+  })
+  .catch(() => {
     console.log('Connection to database failed. Server not started.');
-  }
-};
-initSequelize();
+  });
