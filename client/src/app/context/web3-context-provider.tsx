16575d38f62706254023136f403fd91a5d412d1a
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-10-15 13:54:33 -0400

    Fetch contract factory address/ABI in web3 context

--- client/src/app/context/web3-context-provider.tsx
+++ client/src/app/context/web3-context-provider.tsx
@@ -2,13 +2,20 @@ import { createContext } from 'react';
 import { useState, useEffect } from 'react';
 
 import getWeb3 from '../../utils/web3/web3';
+import getContractFactoryData from '../../utils/web3/getContractFactoryData';
 
 export const Web3Context = createContext<any>({
   web3: null,
+  contractFactoryAddress: null,
+  contractFactoryAbi: null,
 });
 
 const Web3ContextProvider = (props: any) => {
   const [web3, setWeb3] = useState<any>(null);
+  const [contractFactoryAddress, setContractFactoryAddress] = useState<
+    string | null
+  >(null);
+  const [contractFactoryAbi, setContractFactoryAbi] = useState<any>(null);
 
   useEffect(() => {
     const connectEthereum = async () => {
@@ -21,11 +28,30 @@ const Web3ContextProvider = (props: any) => {
         setWeb3(null);
       }
     };
+
+    const getContractFactoryAddress = async () => {
+      if (contractFactoryAddress !== null) {
+        const address = await getContractFactoryData('get-address');
+        setContractFactoryAddress(address);
+      }
+    };
+
+    const getContractFactoryAbi = async () => {
+      if (contractFactoryAbi === null) {
+        const abi = await getContractFactoryData('get-abi');
+        setContractFactoryAbi(abi);
+      }
+    };
+
     connectEthereum();
-  }, [web3]);
+    getContractFactoryAddress();
+    getContractFactoryAbi();
+  }, [web3, contractFactoryAddress, contractFactoryAbi]);
 
   return (
-    <Web3Context.Provider value={{ web3: web3 }}>
+    <Web3Context.Provider
+      value={{ web3, contractFactoryAddress, contractFactoryAbi }}
+    >
       {props.children}
     </Web3Context.Provider>
   );
