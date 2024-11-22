8bb95f0408e5974a82a0b87bc1e0a61cdd84fe9d
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-11-21 19:33:29 -0500

    Updating tests for SharedWallet

--- hardhat/tests/SharedWallet.test.js
+++ hardhat/tests/SharedWallet.test.js
@@ -70,4 +70,137 @@ describe('SharedWallet.sol', () => {
 
     expect(result).not.toBe(null);
   });
+
+  it('should allow deposit of Ether in the contract', async () => {
+    const contractAddress = await testContract.getAddress();
+    await owner.sendTransaction({
+      to: contractAddress,
+      value: ethers.parseEther('10'),
+    });
+
+    const checkBalance = await ethers.provider.getBalance(testContract);
+    const checkBalanceEther = ethers.formatUnits(checkBalance, 'ether');
+    expect(Number(checkBalanceEther)).toBe(10);
+  });
+
+  it('should allow an authorized withdawer to withdraw Ether', async () => {
+    const contractAddress = await testContract.getAddress();
+    await owner.sendTransaction({
+      to: contractAddress,
+      value: ethers.parseEther('10'),
+    });
+
+    await testContract.setWithdrawers([testUser1.address, testUser2.address]);
+
+    const initialUserBalance = await ethers.provider.getBalance(testUser1);
+
+    const withDrawAmount = ethers.parseEther('2');
+    const withdrawResult = await testContract
+      .connect(testUser1)
+      .withdraw(withDrawAmount);
+
+    const contractBalance = await ethers.provider.getBalance(testContract);
+    const finalUserBalance = await ethers.provider.getBalance(testUser1);
+
+    const userBalanceIncrease =
+      Number(ethers.formatUnits(finalUserBalance, 'ether')) -
+      Number(ethers.formatUnits(initialUserBalance, 'ether'));
+    expect(Number(ethers.formatUnits(contractBalance, 'ether'))).toBe(8);
+    expect(userBalanceIncrease).toBeGreaterThan(1.95);
+  });
+
+  it('should revert with an error if unauthorized user attempts to withdraw Ether', async () => {
+    const contractAddress = await testContract.getAddress();
+    await owner.sendTransaction({
+      to: contractAddress,
+      value: ethers.parseEther('10'),
+    });
+
+    await testContract.setWithdrawers([testUser1.address, testUser2.address]);
+
+    const withDrawAmount = ethers.parseEther('2');
+
+    let result;
+    try {
+      result = await testContract.connect(testUser3).withdraw(withDrawAmount);
+    } catch (e) {
+      result = null;
+    }
+    expect(result).toBe(null);
+  });
+
+  it('should revert with an error if withdrawal amount is greater than limit', async () => {
+    const contractAddress = await testContract.getAddress();
+    await owner.sendTransaction({
+      to: contractAddress,
+      value: ethers.parseEther('10'),
+    });
+
+    await testContract.setWithdrawers([testUser1.address, testUser2.address]);
+
+    const withDrawAmount = ethers.parseEther('3');
+
+    let result;
+    try {
+      result = await testContract.connect(testUser2).withdraw(withDrawAmount);
+    } catch (e) {
+      result = null;
+    }
+    expect(result).toBe(null);
+  });
+
+  it('should revert with an error if withdrawal amount is greater than contract balance', async () => {
+    const contractAddress = await testContract.getAddress();
+    await owner.sendTransaction({
+      to: contractAddress,
+      value: ethers.parseEther('1.5'),
+    });
+
+    await testContract.setWithdrawers([testUser1.address, testUser2.address]);
+
+    const withDrawAmount = ethers.parseEther('2');
+
+    let result;
+    try {
+      result = await testContract.connect(testUser2).withdraw(withDrawAmount);
+    } catch (e) {
+      result = null;
+    }
+    expect(result).toBe(null);
+  });
+
+  it('should enable the contract owner to remove withdrawers', async () => {
+    await testContract.setWithdrawers([testUser1.address, testUser2.address]);
+
+    let checkWithdrawer = await testContract.isWithdrawer(testUser1.address);
+    expect(checkWithdrawer).toBe(true);
+
+    const result = await testContract.removeWithDrawers([testUser1.address]);
+
+    checkWithdrawer = await testContract.isWithdrawer(testUser1.address);
+    expect(checkWithdrawer).toBe(false);
+
+    checkWithdrawer = await testContract.isWithdrawer(testUser2.address);
+    expect(checkWithdrawer).toBe(true);
+  });
+
+  it('should allow only the contract owner to remove withdrawers', async () => {
+    await testContract.setWithdrawers([testUser1.address, testUser2.address]);
+
+    let checkWithdrawer = await testContract.isWithdrawer(testUser1.address);
+    expect(checkWithdrawer).toBe(true);
+
+    let result;
+
+    try {
+      await testContract
+        .connect(testUser3)
+        .removeWithDrawers([testUser1.address]);
+      result = null;
+    } catch (e) {
+      result = e;
+    }
+
+    expect(result).not.toBe(null);
+  });
 });
