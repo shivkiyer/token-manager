e275b5a9168037c90c377ca779ecd30895704288
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-11-29 20:58:05 -0500

    Reverting refactoring of manage-waller loader fn

--- client/src/components/dashboard/wallets/manage-wallet/manageWalletLoader.ts
+++ client/src/components/dashboard/wallets/manage-wallet/manageWalletLoader.ts
@@ -1,54 +1,38 @@
 import { redirect } from 'react-router-dom';
 
-import fetchWalletData from './fetch-wallet-data';
-import { clearToken } from '../../../../utils/auth/auth';
-
-// Comments (old code) to be removed after testing
-
-// import apiCall from '../../../../utils/http/api-call';
-// import { authToken, clearToken } from '../../../../utils/auth/auth';
-
-// const manageWalletLoader = async ({ params }: any) => {
-//   const userToken = authToken();
-
-//   if (userToken === null) {
-//     redirect('/login');
-//   }
-
-//   const { id } = params;
-
-//   try {
-//     const response = await apiCall(
-//       `${process.env.REACT_APP_BASE_API_URL}/api/wallets/${id}`,
-//       'GET',
-//       { Authorization: userToken || '' },
-//       null
-//     );
-
-//     const responseData = await response.json();
-//     if (
-//       responseData.message !== null &&
-//       responseData.message !== undefined &&
-//       responseData.message.includes('Authorization failed')
-//     ) {
-//       clearToken();
-//       return redirect('/login');
-//     }
-//     return responseData;
-//   } catch (e) {
-//     return null;
-//   }
-// };
+import apiCall from '../../../../utils/http/api-call';
+import { authToken, clearToken } from '../../../../utils/auth/auth';
 
 const manageWalletLoader = async ({ params }: any) => {
+  const userToken = authToken();
+
+  if (userToken === null) {
+    redirect('/login');
+  }
+
   const { id } = params;
 
-  const data = await fetchWalletData(id);
-  if (data === null) {
-    clearToken();
-    return redirect('/login');
+  try {
+    const response = await apiCall(
+      `${process.env.REACT_APP_BASE_API_URL}/api/wallets/${id}`,
+      'GET',
+      { Authorization: userToken || '' },
+      null
+    );
+
+    const responseData = await response.json();
+    if (
+      responseData.message !== null &&
+      responseData.message !== undefined &&
+      responseData.message.includes('Authorization failed')
+    ) {
+      clearToken();
+      return redirect('/login');
+    }
+    return responseData;
+  } catch (e) {
+    return null;
   }
-  return data;
 };
 
 export default manageWalletLoader;
