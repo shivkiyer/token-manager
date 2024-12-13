f621ced226de6bdecfd58171e37dc65fd4bbbaee
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-12-12 21:21:19 -0500

    Withdraw from wallet complete

--- client/src/components/dashboard/wallets/access-wallet/access-wallet.tsx
+++ client/src/components/dashboard/wallets/access-wallet/access-wallet.tsx
@@ -1,9 +1,7 @@
-import { useState, useEffect, useContext, useCallback } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useLoaderData, useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import RefreshIcon from '@mui/icons-material/Refresh';
 
 import { Web3Context } from './../../../../app/context/web3-context-provider';
 import { clearToken } from '../../../../utils/auth/auth';
@@ -14,13 +12,12 @@ import WithdrawEther from './withdraw-ether';
 
 function AccessWallet() {
   const [error, setError] = useState<string | null>(null);
-  const [activeAccount, setActiveAccount] = useState<string | null>(null);
-  const [accountError, setAccountError] = useState<string | null>(null);
   const walletData: any = useLoaderData();
   const navigate = useNavigate();
   const { web3, sharedWalletAbi } = useContext(Web3Context);
 
   useEffect(() => {
+    setError(null);
     try {
       if (walletData === null || walletData === undefined) {
         Object.assign(new Error());
@@ -33,56 +30,20 @@ function AccessWallet() {
         clearToken();
         navigate('/login');
       }
+      if (
+        walletData.data.address === null ||
+        walletData.data.address === undefined ||
+        sharedWalletAbi === null ||
+        sharedWalletAbi === undefined
+      ) {
+        setError('Wallet could not be fetched');
+        return;
+      }
+      walletData.data.abi = sharedWalletAbi;
     } catch (e) {
       setError('Unable to fetch wallet');
     }
-  }, [walletData, navigate]);
-
-  const fetchActiveAccount = useCallback(async () => {
-    if (web3 !== null && web3 !== undefined) {
-      const accounts = await web3.eth.getAccounts();
-      if (accounts.length > 0) {
-        setActiveAccount(accounts[0]);
-        setAccountError(null);
-      } else {
-        setAccountError('No linked Metamask account found');
-      }
-    }
-  }, [web3]);
-
-  const verifyAccountAccess = useCallback(async () => {
-    if (web3 !== null && web3 !== undefined) {
-      await fetchActiveAccount();
-
-      const userAccounts = walletData.data.user.map(
-        (item: { id: number; address: string; name: string }) => item.address
-      );
-      if (activeAccount !== null && !userAccounts.includes(activeAccount)) {
-        setAccountError('Linked Metamask account is not a wallet user');
-      }
-    }
-  }, [web3, walletData, activeAccount, fetchActiveAccount]);
-
-  useEffect(() => {
-    setError(null);
-    if (web3 === null || web3 === undefined) {
-      setError('Metamask needs to be unlocked to manage the wallet');
-      return;
-    }
-
-    if (
-      walletData.data.address === null ||
-      walletData.data.address === undefined ||
-      sharedWalletAbi === null ||
-      sharedWalletAbi === undefined
-    ) {
-      setError('Wallet could not be fetched');
-      return;
-    }
-    walletData.data.abi = sharedWalletAbi;
-
-    verifyAccountAccess();
-  }, [web3, walletData, sharedWalletAbi, activeAccount, verifyAccountAccess]);
+  }, [walletData, sharedWalletAbi, navigate]);
 
   return (
     <>
@@ -97,24 +58,6 @@ function AccessWallet() {
               <h4>Wallet Users:</h4>
             </Grid>
             <UsersTable users={walletData.data.user} form={null} />
-
-            {accountError && (
-              <>
-                <Grid item xs={12} md={10} marginTop={1}>
-                  <p className='error-message' style={{ textAlign: 'left' }}>
-                    {accountError}
-                  </p>
-                </Grid>
-                <Grid item xs={9} md={7} paddingTop={0.5}>
-                  <p>Change the Metamask account and click on refresh</p>
-                </Grid>
-                <Grid item xs={3} md={3}>
-                  <Button sx={{ padding: '0px' }} onClick={verifyAccountAccess}>
-                    <RefreshIcon />
-                  </Button>
-                </Grid>
-              </>
-            )}
           </Grid>
 
           <WithdrawEther web3={web3} wallet={walletData.data} />
--- client/src/components/dashboard/wallets/access-wallet/withdraw-ether.tsx
+++ client/src/components/dashboard/wallets/access-wallet/withdraw-ether.tsx
@@ -1,12 +1,53 @@
+import { useState, useCallback, useEffect } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 import isErrorInForm from '../../../../utils/forms/isErrorInForm';
 
 function WithdrawEther({ web3, wallet }: { web3: any; wallet: any }) {
+  const [error, setError] = useState<string | null>(null);
+  const [activeAccount, setActiveAccount] = useState<string | null>(null);
+  const [accountError, setAccountError] = useState<string | null>(null);
+
+  const fetchActiveAccount = useCallback(async () => {
+    if (web3 !== null && web3 !== undefined) {
+      const accounts = await web3.eth.getAccounts();
+      if (accounts.length > 0) {
+        setActiveAccount(accounts[0]);
+        setAccountError(null);
+      } else {
+        setAccountError('No linked Metamask account found');
+      }
+    }
+  }, [web3]);
+
+  const verifyAccountAccess = useCallback(async () => {
+    if (web3 !== null && web3 !== undefined) {
+      await fetchActiveAccount();
+
+      const userAccounts = wallet.user.map(
+        (item: { id: number; address: string; name: string }) => item.address
+      );
+      if (activeAccount !== null && !userAccounts.includes(activeAccount)) {
+        setAccountError('Linked Metamask account is not a wallet user');
+      }
+    }
+  }, [web3, wallet, activeAccount, fetchActiveAccount]);
+
+  useEffect(() => {
+    setError(null);
+    if (web3 === null || web3 === undefined) {
+      setError('Metamask needs to be unlocked to manage the wallet');
+      return;
+    }
+
+    verifyAccountAccess();
+  }, [web3, wallet, activeAccount, verifyAccountAccess]);
+
   const validateForm = () => {
     return Yup.object({
       amount: Yup.number()
@@ -17,12 +58,47 @@ function WithdrawEther({ web3, wallet }: { web3: any; wallet: any }) {
     });
   };
 
+  const submitHandler = async (values: any, resetForm: () => void) => {
+    setError(null);
+    try {
+      if (web3 === null || web3 === undefined) {
+        setError('Metamask is locked or account is unavilable');
+        return;
+      }
+      await verifyAccountAccess();
+      const amountInWei = await web3.utils.toWei(values.amount, 'ether');
+
+      const accounts = await web3.eth.getAccounts();
+      const account = accounts[0];
+
+      const contract = new web3.eth.Contract(wallet.abi, wallet.address);
+      const estimateGas = await contract.methods
+        .withdraw(amountInWei)
+        .estimateGas({ from: account });
+      const actualGas = (estimateGas * BigInt(2)).toString();
+
+      const result = await contract.methods
+        .withdraw(amountInWei)
+        .send({ from: account, gas: actualGas });
+
+      if (
+        result.transactionHash === null &&
+        result.transactionHash === undefined
+      ) {
+        Object.assign(new Error());
+      }
+      resetForm();
+    } catch (e) {
+      setError('Could not withdraw funds.');
+    }
+  };
+
   const withdrawalForm = useFormik({
     initialValues: {
       amount: '',
     },
     validationSchema: validateForm,
-    onSubmit: (values: any) => console.log(values),
+    onSubmit: (values, { resetForm }) => submitHandler(values, resetForm),
   });
 
   const submitDisabled = () => {
@@ -32,32 +108,62 @@ function WithdrawEther({ web3, wallet }: { web3: any; wallet: any }) {
   return (
     <form method='POST' onSubmit={withdrawalForm.handleSubmit}>
       <Grid container>
-        <Grid item xs={12} marginTop={3}>
-          <TextField
-            name='amount'
-            label='Amount in Ether'
-            variant='standard'
-            value={withdrawalForm.values.amount}
-            onChange={withdrawalForm.handleChange}
-            onBlur={withdrawalForm.handleBlur}
-            fullWidth
-          ></TextField>
-        </Grid>
-        <Grid item xs={12} marginTop={1}>
-          <Button
-            variant='contained'
-            type='submit'
-            disabled={submitDisabled()}
-            sx={{ marginRight: '12px' }}
-          >
-            Submit
-          </Button>
-          {withdrawalForm.touched.amount && withdrawalForm.errors.amount && (
-            <span className='error-message'>
-              {withdrawalForm.errors.amount.toString()}
-            </span>
-          )}
-        </Grid>
+        {accountError && (
+          <>
+            <Grid item xs={12} md={10} marginTop={1}>
+              <p className='error-message' style={{ textAlign: 'left' }}>
+                {accountError}
+              </p>
+            </Grid>
+            <Grid item xs={9} md={7} paddingTop={0.5}>
+              <p>Change the Metamask account and click on refresh</p>
+            </Grid>
+            <Grid item xs={3} md={3}>
+              <Button sx={{ padding: '0px' }} onClick={verifyAccountAccess}>
+                <RefreshIcon />
+              </Button>
+            </Grid>
+          </>
+        )}
+        {!accountError && (
+          <>
+            <Grid item xs={12} marginTop={3}>
+              <TextField
+                name='amount'
+                label='Amount in Ether'
+                variant='standard'
+                value={withdrawalForm.values.amount}
+                onChange={withdrawalForm.handleChange}
+                onBlur={withdrawalForm.handleBlur}
+                fullWidth
+              ></TextField>
+            </Grid>
+            <Grid item xs={12} marginTop={1}>
+              <Button
+                variant='contained'
+                type='submit'
+                disabled={submitDisabled()}
+                sx={{ marginRight: '12px' }}
+              >
+                Submit
+              </Button>
+              {withdrawalForm.touched.amount &&
+                withdrawalForm.errors.amount && (
+                  <span className='error-message' style={{ textAlign: 'left' }}>
+                    {withdrawalForm.errors.amount.toString()}
+                  </span>
+                )}
+            </Grid>
+
+            {error && (
+              <Grid item xs={12} marginTop={1}>
+                <p className='error-message' style={{ textAlign: 'left' }}>
+                  {error}
+                </p>
+              </Grid>
+            )}
+          </>
+        )}
       </Grid>
     </form>
   );
