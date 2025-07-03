b8f701c8bfee2d356386c16eeea57978b58ea6c2
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2025-07-03 16:30:14 -0400

    Route protection for dashboard and login pages

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
+import { redirect } from 'next/navigation';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-export default function Dashboard() {
+import { getSession } from '@/actions/auth/session';
+
+export default async function Dashboard() {
+  const token = await getSession();
+  if (!token) {
+    redirect('/login');
+  }
+
   return (
     <Container>
       <Box>
--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
@@ -15,14 +15,28 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { AuthContext } from '@/providers/auth/auth-provider';
 import isErrorInForm from '@/utils/forms/isErrorInForm';
 import loginActionHandler from '@/actions/auth/login';
+import { getSession } from '@/actions/auth/session';
 
 import classes from './page.module.css';
 
 function LoginPage() {
+  const [loginStatus, setLoginStatus] = useState<boolean>(true);
   const [formPending, setFormPending] = useState<boolean>(false);
   const [formError, setFormError] = useState<string | null>(null);
   const router = useRouter();
   const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    const getToken = async () => {
+      const token = await getSession();
+      if (token) {
+        router.push('/dashboard');
+        return;
+      }
+      setLoginStatus(false);
+    };
+    getToken();
+  }, []);
 
   const validateHandler = () => {
     setFormError(null);
@@ -55,73 +69,82 @@ function LoginPage() {
   });
 
   return (
-    <Container>
-      <Box className={classes.LoginBox}>
-        <form onSubmit={formik.handleSubmit}>
-          <Grid container>
-            <Grid size={{ xs: 12 }}>
-              <TextField
-                name='username'
-                variant='standard'
-                placeholder='Username'
-                required
-                className={classes.LoginFields}
-                value={formik.values.username}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              ></TextField>
-              {formik.touched.username && formik.errors.username && (
-                <Typography color='error' variant='body1'>
-                  {formik.errors.username}
-                </Typography>
-              )}
-            </Grid>
+    <>
+      {loginStatus && (
+        <Box className={classes.LoginBox}>
+          <CircularProgress size={80} />
+        </Box>
+      )}
+      {!loginStatus && (
+        <Container>
+          <Box className={`${classes.LoginBox} ${classes.LoginBackground}`}>
+            <form onSubmit={formik.handleSubmit}>
+              <Grid container>
+                <Grid size={{ xs: 12 }}>
+                  <TextField
+                    name='username'
+                    variant='standard'
+                    placeholder='Username'
+                    required
+                    className={classes.LoginFields}
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  ></TextField>
+                  {formik.touched.username && formik.errors.username && (
+                    <Typography color='error' variant='body1'>
+                      {formik.errors.username}
+                    </Typography>
+                  )}
+                </Grid>
 
-            <Grid size={{ xs: 12 }} marginTop={5}>
-              <TextField
-                name='password'
-                variant='standard'
-                type='password'
-                placeholder='Password'
-                required
-                className={classes.LoginFields}
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              ></TextField>
-              {formik.touched.password && formik.errors.password && (
-                <Typography color='error' variant='body1'>
-                  {formik.errors.password}
-                </Typography>
-              )}
-            </Grid>
+                <Grid size={{ xs: 12 }} marginTop={5}>
+                  <TextField
+                    name='password'
+                    variant='standard'
+                    type='password'
+                    placeholder='Password'
+                    required
+                    className={classes.LoginFields}
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  ></TextField>
+                  {formik.touched.password && formik.errors.password && (
+                    <Typography color='error' variant='body1'>
+                      {formik.errors.password}
+                    </Typography>
+                  )}
+                </Grid>
 
-            <Grid size={{ xs: 12 }} marginTop={3}>
-              {formError && (
-                <Typography color='error' variant='body1'>
-                  {formError}
-                </Typography>
-              )}
-            </Grid>
+                <Grid size={{ xs: 12 }} marginTop={3}>
+                  {formError && (
+                    <Typography color='error' variant='body1'>
+                      {formError}
+                    </Typography>
+                  )}
+                </Grid>
 
-            <Grid size={{ xs: 12 }} marginTop={2}>
-              {formPending ? (
-                <CircularProgress size={26} />
-              ) : (
-                <Button
-                  disabled={getDisabledStatus()}
-                  type='submit'
-                  variant='contained'
-                  className={classes.LoginFields}
-                >
-                  <Typography variant='button'>Login</Typography>
-                </Button>
-              )}
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
-    </Container>
+                <Grid size={{ xs: 12 }} marginTop={2}>
+                  {formPending ? (
+                    <CircularProgress size={26} />
+                  ) : (
+                    <Button
+                      disabled={getDisabledStatus()}
+                      type='submit'
+                      variant='contained'
+                      className={classes.LoginFields}
+                    >
+                      <Typography variant='button'>Login</Typography>
+                    </Button>
+                  )}
+                </Grid>
+              </Grid>
+            </form>
+          </Box>
+        </Container>
+      )}
+    </>
   );
 }
 
